c13f0b7ef1f22bdc973d1045fba12051c2bb8cbc
Cruz Sanchez <cruzsanchez9209@gmail.com>
2019-12-16 22:48:11 -0600

    Add project files.

---
+++ ProblematicProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProblematicProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
