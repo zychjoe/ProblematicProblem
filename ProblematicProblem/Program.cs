24e5bb3c73f5853249d2e02a4e48d8937642909f
Cruz Sanchez <41009730+CruzSanchez@users.noreply.github.com>
2020-01-11 22:10:29 -0600

    Update Program.cs
    Added Logical error

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -88,7 +88,7 @@ ProblematicProblem
 
                 string randomActivity = activities[randomNumber]
 
-                if (userAge < 21 && randomActivity == "Wine Tasting")
+                if (userAge > 21 && randomActivity == "Wine Tasting")
                 {
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
                     Console.WriteLine("Pick something else!");
